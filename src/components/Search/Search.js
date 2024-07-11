b7b4fdacf2efa69191a6f550c8551f3e3232dbdf
Muhammad Yasir Amri <yasiramrr@gmail.com>
2024-07-11 21:16:36 +0700

    add logic for search bar

---
+++ src/components/Search/Search.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import styles from "./Search.module.scss";
+
+const Search = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm.trim());
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mb-4">
+      <div className="d-flex justify-content-center gap-4 mb-5">
+        <input
+          type="text"
+          className={`form-control ${styles.input}`}
+          placeholder="Cari karakter..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <div className="input-group-append">
+          <button
+            className={`${styles.btn} btn btn-success fs-5`}
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Search;
