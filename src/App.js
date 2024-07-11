2e7533dba20f635710ba02faaab6a83166f7c000
Muhammad Yasir Amri <yasiramrr@gmail.com>
2024-07-11 17:57:26 +0700

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      console.log(data);
+    })();
+  }, [api]);
   return (
     <div className="App">
       <h1 className="text-center text-success ubuntu my-4">
         MOSTRANS ASSIGMENT
       </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
