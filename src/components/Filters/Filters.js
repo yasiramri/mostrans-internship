b802c156b44a1344177b28e9a8301649db10ac64
Muhammad Yasir Amri <yasiramrr@gmail.com>
2024-07-12 07:48:29 +0700

    menyesuaikan filters dengan category dan button

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,9 +1,27 @@
-import React from "react";
+// /src/components/Filters/Filters.js
 
-const Filters = () => {
+import React from "react";
+import Status from "../Filters/Category/Status";
+import Gender from "../Filters/Category/Gender";
+import Species from "../Filters/Category/Species";
+
+const Filters = ({ setStatus, setGender, setSpecies, clearFilters }) => {
   return (
-    <div className="col-3">
-      <div className="text-center">Filter</div>
+    <div className="col-4">
+      <div className="text-center fw-bold fs-4 mb-2">Filters</div>
+      <div className="accordion" id="accordionExample">
+        <Status setStatus={setStatus} />
+        <Gender setGender={setGender} />
+        <Species setSpecies={setSpecies} />
+      </div>
+
+      <div
+        style={{ cursor: "pointer" }}
+        className="text-center text-success text-decoration-underline mb-4"
+        onClick={clearFilters}
+      >
+        Clear Filters
+      </div>
     </div>
   );
 };
